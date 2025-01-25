e32ae503623173f9ccbb63a3f863b8cc70a8ef91
Alberto Aguilar <93626043+aguilaral24@users.noreply.github.com>
2025-01-24 20:30:10 -0600

    Se agrega caso de uso para Eje X con datos definidos

--- bibliotecaVDA/04. XAxisFromArray/main.js
+++ bibliotecaVDA/04. XAxisFromArray/main.js
@@ -1,1 +1,32 @@
 
+import { initViewport, drawXAxisWithIntervals, loadCSV } from '../vda.js';
+
+// Función principal para inicializar el canvas y dibujar el eje X
+async function init() {
+    
+    //Init viewPort
+    const canvasId = 'miCanvas';
+    const width = 1550;
+    const height = 950;
+    const context = initViewport(canvasId, width, height);
+
+    //Carga de datos desde CSV
+    const data = await loadCSV("/data/xAxisData.csv");
+    const xColumnName = "x"
+    let xValues = data.map(row => row[xColumnName]);
+
+    //Parametros para el eje X
+    const xPos = 0;
+    const yPos = 0;
+    const color = "black";
+    const labelSpace = 20;
+    const canvasPadding = 50
+    const xLabelTextAngle = 0;
+    const interval = 0;
+    
+    // Llamar a la función para dibujar el eje X
+    drawXAxisWithIntervals(context, xValues , xPos, yPos, xLabelTextAngle, color, labelSpace, canvasPadding, interval);
+}
+
+// Llama a la función principal para inicializar todo
+init();
