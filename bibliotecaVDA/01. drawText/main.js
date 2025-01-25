16bb55a964309eef0aa8d8dc8120e4e05f2a5ae6
Alberto Aguilar <93626043+aguilaral24@users.noreply.github.com>
2025-01-24 20:26:23 -0600

    Se agrega caso de uso para draw text

--- bibliotecaVDA/01. drawText/main.js
+++ bibliotecaVDA/01. drawText/main.js
@@ -1,1 +1,17 @@
+import { initViewport, drawText } from '../vda.js';
 
+document.addEventListener('DOMContentLoaded', () => {
+  
+  // Llamamos a la función initViewPort de la biblioteca vda.js 
+  const width = 1150;
+  const height = 10000;
+  const context = initViewport('miCanvas', width, height);
+
+  const title = "Título de ejemplo";
+  const canvasPadding = 50;
+  const angleText = 0;
+  const size = 50;
+  const color = "darkcyan" ;
+  drawText(context,title, width/2 + canvasPadding ,height - canvasPadding, size ,color,- angleText)
+
+});
